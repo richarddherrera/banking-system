009e9f898e53a465a7767260c723c53154f0058e
Richard Herrera <yrichardz3003@gmail.com>
2025-08-08 16:50:49 -0300

    refactor(service): Best methods for listing

--- src/service/BankingSystem.java
+++ src/service/BankingSystem.java
@@ -4,9 +4,11 @@ import model.Account;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BankingSystem {
-    private final List<Account> accountList = new ArrayList<>();
+
+    private List<Account> accountList = new ArrayList<>();
 
     public void addAccount(Account account){
         accountList.add(account);
@@ -16,8 +18,31 @@ public class BankingSystem {
         accountList.remove(account);
     }
 
-    public void listAccounts(Account account){
+    public void listAccounts(){
         accountList.forEach(System.out::println);
     }
 
+    public boolean lessTwo(){
+        boolean confirm = false;
+        if (accountList.size() < 2){
+            confirm = true;
+            System.out.println("You need to create two accounts, create them by typing [2].");
+        }
+        return confirm;
+    }
+
+    public List<Account> listDifferentAccount(int number){
+        return accountList.stream().filter(account -> account.getNumber() != number).toList();
+    }
+
+    public Account findByAccountNumber(int numberAccount){
+
+       return accountList.stream()
+               .filter(account -> Objects.equals(account.getNumber(), numberAccount))
+               .findFirst().orElse(null);
+    }
+
+    public List<Account> getAccountList() {
+        return accountList;
+    }
 }
