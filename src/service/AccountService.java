4000dabdf2a16c51f47d418e13fcd8e8ec93bfcd
Richard Herrera <yrichardz3003@gmail.com>
2025-08-08 17:14:38 -0300

    feat(service): add in AccountService method withdraw

--- src/service/AccountService.java
+++ src/service/AccountService.java
@@ -1,10 +1,19 @@
 package service;
 
+import exceptions.AccountWithdrawException;
 import model.Account;
 
 public class AccountService {
 
     public void deposit(Account account, double amount){
         account.setBalance(account.getBalance() + amount);
+    }
+
+    public void withdraw(Account account, double amount){
+        if (amount > account.getBalance()){
+            throw new AccountWithdrawException("\n ⚠ The number account cannot be empty or null! \n");
+        }else {
+            account.setBalance(account.getBalance() - amount);
+        }
     }
 }
