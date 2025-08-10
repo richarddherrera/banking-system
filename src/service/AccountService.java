5594c44a3d2a59447e9dae831d4a21142e259e71
Richard Herrera <yrichardz3003@gmail.com>
2025-08-10 09:47:47 -0300

    feat(service): add in AccountService method withdraw

--- src/service/AccountService.java
+++ src/service/AccountService.java
@@ -1,5 +1,6 @@
 package service;
 
+import exceptions.AccountTransferException;
 import exceptions.AccountWithdrawException;
 import model.Account;
 
@@ -11,9 +12,18 @@ public class AccountService {
 
     public void withdraw(Account account, double amount){
         if (amount > account.getBalance()){
-            throw new AccountWithdrawException("\n ⚠ The number account cannot be empty or null! \n");
+            throw new AccountWithdrawException("\n ⚠ The amount entered is greater than the balance in the account! \n");
         }else {
             account.setBalance(account.getBalance() - amount);
+        }
+    }
+
+    public void transfer(Account accountSender, Account accountRecipient, Double amount){
+        if (amount > accountSender.getBalance()){
+            throw new AccountTransferException("\n ⚠ The amount entered is greater than the balance in the account! \n");
+        }else {
+            accountSender.setBalance(accountSender.getBalance() - amount);
+            accountRecipient.setBalance(accountRecipient.getBalance() + amount);
         }
     }
 }
